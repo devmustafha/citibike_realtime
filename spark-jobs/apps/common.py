058456d7b196f6ae215f2865ce819232e763b716
Mustapha Shuaib <dev.ozovehe@gmail.com>
2026-07-30 16:00:45 +0100

    chore(spark): Remove spark-jobs folder, restructure spark

--- spark-jobs/apps/common.py
+++
@@ -1,7 +0,0 @@
-from pyspark.sql import SparkSession
-
-
-def create_spark_session() -> SparkSession:
-    return (
-        SparkSession.builder.appName("citibike-bronze").master("local[*]").getOrCreate()
-    )
