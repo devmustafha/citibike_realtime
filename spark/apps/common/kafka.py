e6abde1faeb879937d60e7cd707fb1cb0d26ff14
Mustapha Shuaib <dev.ozovehe@gmail.com>
2026-07-31 05:39:10 +0100

    refactor(bronze): Make read kafka stream reuseable

---
+++ spark/apps/common/kafka.py
@@ -0,0 +1,12 @@
+from pyspark.sql import DataFrame, SparkSession
+
+
+def read_kafka_stream(
+    spark: SparkSession, bootstrap_servers: str, topic: str
+) -> DataFrame:
+    return (
+        spark.readStream.format("kafka")
+        .option("kafka.bootstrap.servers", bootstrap_servers)
+        .option("subscribe", topic)
+        .load()
+    )
