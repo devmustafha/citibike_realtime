19f02976ced8204736585f3e59538738f4258dcd
Mustapha Shuaib <dev.ozovehe@gmail.com>
2026-08-08 16:05:20 +0100

    refactor(config): setup default config values

--- common/config.py
+++ common/config.py
@@ -4,21 +4,29 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    citibike_api_url: str
+    citibike_api_url = "https://gbfs.lyft.com/gbfs/2.3/bkn/fr/station_status.json"
     kafka_bootstrap_servers: str
-    poll_interval_seconds: int = 30
-    kafka_station_status_topic: str = "station-status"
-    bronze_station_status_path: str
-    silver_station_status_path: str
-    silver_station_checkpoint_path: str
+    poll_interval_seconds = 30
+    kafka_station_status_topic = "station-status"
+    bronze_station_status_path = "s3a://bronze/station_status"
+    silver_station_status_path = "s3a://silver/station_status"
+    silver_station_checkpoint_path = "s3a://silver/station_status/checkpoint"
+
     minio_endpoint: str
     minio_access_key: str
     minio_secret_key: str
     minio_secure: bool = False
-    latest_station_status_path: str = "s3a://gold/latest_station_status"
-    station_hourly_metrics_path: str = "s3a://gold/station_hourly"
-    station_daily_metrics_path: str = "s3a://gold/station_daily"
-    system_metrics_path: str = "s3a://gold/system_metrics"
+
+    latest_station_status_path = "s3a://gold/latest_station_status"
+
+    station_hourly_metrics_path = "s3a://gold/station_hourly"
+
+    station_daily_metrics_path = "s3a://gold/station_daily"
+
+    system_metrics_path = "s3a://gold/system_metrics"
+
+    telegram_bot_token: str
+    telegram_chat_id: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
